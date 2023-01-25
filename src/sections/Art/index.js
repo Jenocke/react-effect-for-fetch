fed24d7da00e7b0b123b65c3833952f8fd9d0a05
Jenocke <jenocke@gmail.com>
2023-01-25 23:23:50 +0100

    art section - fetching artwork list

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react"
+import Artwork from "./Artwork.js"
 
 function ArtsSection() {
+  const [artworksList, setArtworksList] = useState(null);
+
+  useEffect(() =>{
+    fetch("https://api.artic.edu/api/v1/artworks")
+      .then(res => res.json())
+      .then(data => {
+        setArtworksList(data.data)
+      });
+    }, []
+  )
+  
+
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+        <ul class="art-list">
+          {artworksList.map(item => {
+            <>
+            <Artwork artwork = {item} />
+            </>
+          })}
+        </ul>
+      </div>
     </section>
   )
 }
